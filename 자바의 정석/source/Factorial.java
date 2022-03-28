6678747d6983b4001091168f971f52afbcffb40b
chlrkdgml77 <100984229+chlrkdgml77@users.noreply.github.com>
2022-03-29 08:02:32 +0900

    재귀메서드

---
+++ 자바의 정석/source/Factorial.java
@@ -0,0 +1,22 @@
+package ME_01;
+
+public class Factorial {
+	public static void main(String[] args) {
+		int n = 21;
+		long result = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			result = factorial(i);
+			
+			if (result == -1) {
+				break;
+			}
+			System.out.printf("%2d! = %20d\n", i, result);
+		}
+	}
+	public static long factorial(int n) {
+		if (n <= 0 || n > 20) {return -1;}
+		if (n <= 1) {return 1;}
+		return n * factorial(n-1);
+	}
+}
